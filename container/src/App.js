bce208aaf6b58feaf6c5bbc347a4b06005bff86f
farzadalizadehdev <farzadalizadehdev@gmail.com>
2022-03-25 23:01:37 +0430

    shared dependencies

---
+++ container/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
+export default () => {
+  return (
+    <div>
+      <h1>CONTAINER</h1>
+      <hr />
+      <MarketingApp />
+    </div>
+  );
+};
